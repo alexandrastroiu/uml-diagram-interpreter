41307a5df0734440dd3beb8f32ff73c0c52254d9
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2026-07-22 12:40:51 +0300

    Add class for reading files
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ uml-diagram-parser/src/io/FileReader.java
@@ -0,0 +1,28 @@
+package io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+
+public class FileReader {
+
+    // Default Constructor
+    public FileReader() {}
+
+    public List<String> readFileIntoList(String fileName) {
+        List<String> lines = Collections.emptyList();
+        Path filePath = Paths.get(fileName);
+
+        try {
+            lines = Files.readAllLines(filePath);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        return lines;
+    }
+
+}
