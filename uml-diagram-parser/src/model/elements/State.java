38cc065f3f2d147cb908ebad1bf963c75a1b5aaf
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2026-07-24 18:50:24 +0300

    Add state class constructor
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

--- uml-diagram-parser/src/model/elements/State.java
+++ uml-diagram-parser/src/model/elements/State.java
@@ -2,13 +2,26 @@ package model.elements;
 
 import enums.StateType;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class State {
 
     private String name;
+    private String description;
     private StateType type;
     private Set<State> substates;
+
+    // Constructor
+
+    public State() {
+        this.name = "";
+        this.description = "";
+        this.type = StateType.SIMPLE;
+        this.substates = new HashSet<State>();
+    }
+
+    // Setters, Getters
 
     public String getName() {
         return name;
@@ -32,5 +45,13 @@ public class State {
 
     public void setSubstates(Set<State> substates) {
         this.substates = substates;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
