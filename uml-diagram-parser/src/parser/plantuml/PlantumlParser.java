3db09d274c9da204f28e820221161e8f052a8757
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2026-07-24 18:51:45 +0300

    Add initial implementation for diagram parser
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

--- uml-diagram-parser/src/parser/plantuml/PlantumlParser.java
+++ uml-diagram-parser/src/parser/plantuml/PlantumlParser.java
@@ -2,10 +2,15 @@ package parser.plantuml;
 
 import enums.DiagramType;
 import enums.Language;
+import enums.StateType;
+import model.diagrams.StateDiagram;
 import model.diagrams.UmlDiagram;
+import model.elements.State;
 import parser.DiagramParser;
 
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 public class PlantumlParser implements DiagramParser {
 
@@ -14,10 +19,60 @@ public class PlantumlParser implements DiagramParser {
         if (language.equals(Language.PLANTUML)) {
             switch (type) {
                 case STATE:
+                    StateDiagram stateDiagram = new StateDiagram();
+                    stateDiagram.setLanguage(Language.PLANTUML);
+                    stateDiagram.setType(DiagramType.STATE);
+                    stateDiagram.setLinesCount(lines.size());
+
+                    Map<String, StateType> stereotypes = Map.ofEntries(
+                            Map.entry("<<start>>", StateType.INITIAL),
+                            Map.entry("<<end>>", StateType.FINAL),
+                            Map.entry("<<choice>>", StateType.CHOICE),
+                            Map.entry("fork", StateType.FORK),
+                            Map.entry("join", StateType.JOIN)
+                    );
+
                     for (String line : lines) {
                         if (!line.isBlank()) {
                             String trimmedLine = line.trim();
 
+                            if (trimmedLine.startsWith("[*] ")) {
+                                State initialState = new State();
+
+                                initialState.setName("Initial State");
+                                initialState.setType(StateType.INITIAL);
+
+                                stateDiagram.getStates().add(initialState);
+                            }
+
+                            if (trimmedLine.endsWith("[*] ")) {
+                                State finalState = new State();
+
+                                finalState.setName("Final State");
+                                finalState.setType(StateType.FINAL);
+
+                                stateDiagram.getStates().add(finalState);
+                            }
+
+                            if (trimmedLine.startsWith("state ")) {
+                                if (Pattern.matches("\\bstate .+ as .+\\b", trimmedLine)) {
+                                    //TODO description
+                                }
+
+                                if (trimmedLine.endsWith("{")) {
+                                    //TODO composite state + substates
+                                }
+
+                                for (String key : stereotypes.keySet()) {
+                                    if (trimmedLine.endsWith(key)) {
+                                        //TODO stereotype
+                                    }
+                                }
+                            }
+
+                            if (Pattern.matches("\\b.+ -[a-zA-Z,#\\[\\]]*-> .+ \\b", trimmedLine)) {
+                                //TODO transition
+                            }
                         }
                     }
                     break;
