545f0c75d3ac8f3c263947877a360fcddbb3c943
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2026-07-28 12:51:25 +0300

    Fix state diagram parser
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

--- uml-diagram-parser/src/parser/plantuml/PlantumlParser.java
+++ uml-diagram-parser/src/parser/plantuml/PlantumlParser.java
@@ -22,71 +22,7 @@ public class PlantumlParser implements DiagramParser {
         if (language.equals(Language.PLANTUML)) {
             switch (type) {
                 case STATE:
-                    StateDiagram stateDiagram = new StateDiagram();
-                    stateDiagram.setLanguage(Language.PLANTUML);
-                    stateDiagram.setType(DiagramType.STATE);
-                    stateDiagram.setLinesCount(lines.size());
-
-                    Map<String, StateType> stereotypes = Map.ofEntries(
-                            Map.entry("<<start>>", StateType.INITIAL),
-                            Map.entry("<<end>>", StateType.FINAL),
-                            Map.entry("<<choice>>", StateType.CHOICE),
-                            Map.entry("fork", StateType.FORK),
-                            Map.entry("join", StateType.JOIN)
-                    );
-
-                    for (String line : lines) {
-                        if (!line.isBlank()) {
-                            String trimmedLine = line.trim();
-
-                            if (trimmedLine.startsWith("[*] ")) {
-                                State initialState = new State();
-
-                                initialState.setName("Initial State");
-                                initialState.setType(StateType.INITIAL);
-
-                                stateDiagram.getStates().add(initialState);
-                            }
-
-                            if (trimmedLine.endsWith("[*] ")) {
-                                State finalState = new State();
-
-                                finalState.setName("Final State");
-                                finalState.setType(StateType.FINAL);
-
-                                stateDiagram.getStates().add(finalState);
-                            }
-
-                            if (trimmedLine.startsWith("state ")) {
-                                State state = new State();
-
-                                if (Pattern.matches("\\bstate .+ as .+\\b", trimmedLine)) {
-                                    //TODO description
-                                }
-
-                                if (trimmedLine.endsWith("{")) {
-                                    state.setType(StateType.COMPOSITE);
-                                    //TODO composite state + substates
-                                }
-
-                                for (String key : stereotypes.keySet()) {
-                                    if (trimmedLine.endsWith(key)) {
-                                        state.setType(stereotypes.get(key));
-                                        //TODO stereotype
-                                    }
-                                }
-                            }
-
-                            if (Pattern.matches("\\b.+ -[a-zA-Z,#\\[\\]]*-> .+ \\b", trimmedLine)) {
-                                //TODO transition
-                                Transition transition = new Transition();
-
-                                stateDiagram.getTransitions().add(transition);
-                            }
-                        }
-                    }
-
-                    return stateDiagram;
+                    return parseStateDiagram(lines);
                 case ACTIVITY:
                     //TODO
                     break;
@@ -98,6 +34,136 @@ public class PlantumlParser implements DiagramParser {
             }
         }
         return umlDiagram;
+    }
+
+    public StateDiagram parseStateDiagram(List<String> lines) {
+        StateDiagram stateDiagram = new StateDiagram();
+        stateDiagram.setLanguage(Language.PLANTUML);
+        stateDiagram.setType(DiagramType.STATE);
+        stateDiagram.setLinesCount(lines.size());
+
+        Map<String, StateType> stereotypes = Map.ofEntries(
+                Map.entry("<<start>>", StateType.INITIAL),
+                Map.entry("<<end>>", StateType.FINAL),
+                Map.entry("<<choice>>", StateType.CHOICE),
+                Map.entry("<<fork>>", StateType.FORK),
+                Map.entry("<<join>>", StateType.JOIN)
+        );
+
+        String initialPattern = "[*] ";
+        String finalPattern = " [*]";
+        String statePattern = "state ";
+        String descriptionPattern = " as ";
+        String colorPattern = " #";
+        String compositePattern = " {";
+        String longNamePattern = "^state [\\s\\S]+ as [\\s\\S]+$";
+        String transitionPattern = "^[\\s\\S]+ -[a-zA-Z,#\\[\\]]*-> [\\s\\S]+$";
+        String startPattern = " -";
+        String endPattern = "-> ";
+        String stateDescription = " :";
+
+        for (String line : lines) {
+            if (!line.isBlank()) {
+                String trimmedLine = line.trim();
+
+                // Stare
+                if (trimmedLine.startsWith(statePattern)) {
+                    State state = new State();
+                    int index1 = statePattern.length();
+                    String name = trimmedLine.substring(index1);
+
+                    if (Pattern.matches(longNamePattern, trimmedLine)) {
+                        int index2 = trimmedLine.indexOf(descriptionPattern);
+                        int index3 = index2 + longNamePattern.length();
+
+                        String description = trimmedLine.substring(index1, index2).trim();
+                        name = trimmedLine.substring(index3).trim();
+
+                        state.setDescription(description);
+                    }
+
+                    if (trimmedLine.endsWith(compositePattern)) {
+                        int index6 = name.indexOf(compositePattern);
+                        name = name.substring(0, index6).trim();
+                        //TODO Substates
+
+                        state.setType(StateType.COMPOSITE);
+                    }
+
+                    if (trimmedLine.contains(colorPattern)) {
+                        int index5 = name.indexOf(colorPattern);
+                        name = name.substring(0, index5).trim();
+                    }
+
+                    for (String key : stereotypes.keySet()) {
+                        if (trimmedLine.contains(key)) {
+                            int index4 = name.indexOf(key);
+                            name = name.substring(0, index4).trim();
+
+                            state.setType(stereotypes.get(key));
+                        }
+                    }
+
+                    state.setName(name);
+                    stateDiagram.addState(state);
+                }
+
+                // Tranzitie
+                if (Pattern.matches(transitionPattern, trimmedLine)) {
+                    Transition transition = new Transition();
+                    State startState = new State();
+                    State endState = new State();
+                    String name;
+
+                    if (trimmedLine.startsWith(initialPattern)) {
+                        startState.setName("Initial State");
+                        startState.setType(StateType.INITIAL);
+                    }
+                    else {
+                        int indexStart = trimmedLine.indexOf(startPattern);
+
+                        name = trimmedLine.substring(0, indexStart).trim();
+                        startState.setName(name);
+                    }
+
+                    int indexEnd = trimmedLine.indexOf(endPattern);
+                    int indexStateEnd = indexEnd + endPattern.length();
+
+                    if (trimmedLine.contains(stateDescription)) {
+                        int indexDescription = trimmedLine.indexOf(stateDescription);
+                        name = trimmedLine.substring(indexStateEnd, indexDescription).trim();
+                        String description = trimmedLine.substring(indexDescription + stateDescription.length()).trim();
+
+                        transition.setTransitionDescription(description);
+                    }
+                    else {
+                        name = trimmedLine.substring(indexStateEnd).trim();
+                    }
+
+                    if (name.endsWith(finalPattern)) {
+                        endState.setName("Final State");
+                        endState.setType(StateType.FINAL);
+                    } else {
+                        endState.setName(name);
+                    }
+
+                    transition.addTransitionStates(stateDiagram, startState, endState);
+                    stateDiagram.addState(startState);
+                    stateDiagram.addState(endState);
+                    stateDiagram.getTransitions().add(transition);
+                }
+            }
+        }
+
+        stateDiagram.setTransitionCount(stateDiagram.getTransitions().size());
+        stateDiagram.setRelationships(stateDiagram.getTransitionCount());
+        stateDiagram.setForkCount(stateDiagram.countNodes(StateType.FORK));
+        stateDiagram.setJoinCount(stateDiagram.countNodes(StateType.JOIN));
+        stateDiagram.setChoiceStates(stateDiagram.countNodes(StateType.CHOICE));
+        stateDiagram.setCompositeStates(stateDiagram.countNodes(StateType.COMPOSITE));
+        stateDiagram.setElements(stateDiagram.countElements());
+
+        return stateDiagram;
     }
 
 }
