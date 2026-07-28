27a983a5c8596dc3be6819925baff7af676611b8
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2026-07-28 12:52:10 +0300

    Update main class
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

--- uml-diagram-parser/src/Main.java
+++ uml-diagram-parser/src/Main.java
@@ -1,6 +1,10 @@
 import detector.DiagramDetector;
 import detector.LanguageDetector;
+import enums.DiagramType;
+import enums.Language;
 import io.FileReader;
+import model.diagrams.StateDiagram;
+import parser.plantuml.PlantumlParser;
 
 import java.util.List;
 
@@ -10,10 +14,29 @@ public class Main {
         FileReader f = new FileReader();
         LanguageDetector l = new LanguageDetector();
         DiagramDetector d = new DiagramDetector();
+        PlantumlParser p = new PlantumlParser();
 
         // Test
-        List<String> lines = f.readFileIntoList("test_diagram.txt");
-        System.out.println("Diagram Language: " + l.detectDiagramLanguage(lines));
-        System.out.println("Diagram Type: " + d.detectDiagramType(lines, l.detectDiagramLanguage(lines)));
+        List<String> lines = f.readFileIntoList("test.txt");
+        System.out.println("Limbaj: " + l.detectDiagramLanguage(lines));
+        Language lang = l.detectDiagramLanguage(lines);
+        System.out.println("Tipul Diagramei: " + d.detectDiagramType(lines, l.detectDiagramLanguage(lines)));
+        DiagramType type = d.detectDiagramType(lines, l.detectDiagramLanguage(lines));
+        System.out.println();
+
+        // Test parser
+        StateDiagram stateDiagram = (StateDiagram)p.parseDiagram(lines, lang, type);
+        System.out.println("Stari:");
+        stateDiagram.printStates();
+        System.out.println("Numarul de elemente: ");
+        System.out.println(stateDiagram.getElements());
+        System.out.println();
+        System.out.println("Tranzitii:");
+        stateDiagram.printTransitions();
+        System.out.println("Numarul de tranzitii: ");
+        System.out.println(stateDiagram.getRelationships());
+        System.out.println();
+        System.out.println("Linii de Cod:");
+        System.out.println(stateDiagram.getLinesCount());
     }
 }
